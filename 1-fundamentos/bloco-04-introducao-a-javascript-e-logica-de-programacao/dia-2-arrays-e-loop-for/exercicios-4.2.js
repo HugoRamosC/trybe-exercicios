729efacb827a47a9021583a0f8b02f88eedc91af
HugoRamosC <hugor.castilho@gmail.com>
2022-07-21 19:31:40 -0300

    add resolucao das outras questoes

--- 1-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-arrays-e-loop-for/exercicios-4.2.js
+++ 1-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-arrays-e-loop-for/exercicios-4.2.js
@@ -1,5 +1,71 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (const list of numbers) {
-  console.log(list);
-}
+
+// QUESTAO 1.
+// for (const list of numbers) {
+//   console.log(list);
+// }
+
+// QUESTAO 2.
+// let somaArray = 0
+
+// for (let index = 0; index < numbers.length; index++) {
+  
+//   somaArray = somaArray + numbers[index];
+  
+// }
+// console.log(somaArray);
+
+// // QUESTAO 3.
+// let mediaArray = somaArray / numbers.length
+// console.log(mediaArray);
+
+// // QUESTAO 4.
+// if (mediaArray > 20) {
+//   console.log('Valor maior que 20, valor igual a ',mediaArray);
+// } else {
+//   console.log('Valor menor ou igual a 20, valor igual a ',mediaArray);
+// } 
+
+// // QUESTAO 5.
+// let maiorValor = 0
+// for (let index = 0; index < numbers.length; index++) {
+//   if (numbers[index] > maiorValor) {
+//     maiorValor = numbers[index];
+//   }
+// } 
+// console.log(maiorValor);
+
+// QUESTAO 6.
+// let impars = 0
+// for (let index = 0; index < numbers.length; index++) {
+//   if (numbers[index] % 2 !== 0) {
+//     impars++ ;
+//   } else if (impars === 0){
+//     console.log("nenhum valor ímpar encontrado");
+// }  
+// console.log(impars);
+
+// // QUESTAO 7.
+// let menorValor = numbers[0]
+// for (let index = 0; index < numbers.length; index++) {
+//   if (numbers[index] < menorValor) {
+//     menorValor = numbers[index];
+//   }
+// } 
+// console.log(menorValor);
+
+// // QUESTAO 8.
+// let array25 = []
+// for (let index = 0; index <= 25; index++) {
+//   array25.push(index);
+// }
+// console.log(array25);
+
+// // QUESTAO 9.
+// let arrayDiv2 = []
+// for (let index = 0; index < array25.length; index++) {
+//   arrayDiv2.push(array25[index] / 2)
+  
+// }
+// console.log(arrayDiv2);
