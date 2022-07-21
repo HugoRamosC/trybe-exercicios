b5ddf3f42fe8293cbeca8bcf14104101d69ee235
HugoRamosC <hugor.castilho@gmail.com>
2022-07-21 18:15:27 -0300

    exercios do conteudo

---
+++ 1-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exerc-conteudo.js
@@ -0,0 +1,90 @@
+// const myName = "Hugo Ramos"
+// const birthCity = "Niterói"
+// let birthYear = 1989
+
+// console.log(myName);
+// console.log(birthCity);
+// console.log(birthYear);
+
+// birthYear = 2030
+
+// console.log(birthYear);
+
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientId);
+// console.log(typeof isEnrolled);
+// console.log(typeof patientInfo);
+// console.log(typeof patientEmail);
+
+// const base = 5;
+// let height = 8;
+// const area = base * height;
+// const perimeter = base * 2 + height * 2;
+
+// console.log(base);
+// console.log(height);
+// console.log(area);
+// console.log(perimeter);
+
+// const notaCandidata = 65
+
+// if (notaCandidata >= 80){
+//   console.log ("Parabens, voce foi aprovada!");
+// } else if (notaCandidata < 80 && notaCandidata >= 60){
+//   console.log ("Voce esta na lista de espera.");
+// } else {
+//   console.log ("Voce foi reprovada.");
+// }
+
+// let statusCandidata = "tente"
+
+// switch (statusCandidata) {
+//   case "aprovada":
+//   console.log ("Parabens, voce foi aprovada!");
+//     break;
+
+//     case "lista":
+//   console.log ("Voce esta na lista de espera.");
+//     break;
+
+//     case "reprovada":
+//   console.log ("Voce foi reprovada.");
+//     break;
+
+//   default:
+//     console.log("não se aplica")
+//     break;
+// }
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+
+// menu.push('Contato');
+
+// console.log(menu);
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for (let index = 0; index < groceryList.length; index++) {
+//   console.log(groceryList[index]);
+  
+// }
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let nomes of names) {
+  console.log(nomes);
+}
