897c31141c13cf08ef5b7e7914c713f224d0ce38
HugoRamosC <hugor.castilho@gmail.com>
2022-07-23 14:00:40 -0300

    organizacao das pastas

---
+++ 1-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-arrays-e-loop-for/extras.js
@@ -0,0 +1,50 @@
+// let array = []
+// for (let index = 1; index <= 100; index++) {
+//   array.push (index);
+// }
+// console.log(array);
+
+// for (const numeros of array) {
+//   if (numeros % 2 != 0)  {
+//     console.log(numeros);
+//   }
+// }
+
+// let div3 = [];
+// for (let index = 0; index < array.length; index++) {
+//   if (array[index] % 3 === 0) {
+//     div3.push (array[index])
+//   }
+// }
+// console.log(div3);
+
+// console.log(array.indexOf(45));
+
+// let variavel = 
+// for (let index = 0; index < array.length; index++) {
+//   if (array[index] === variavel ) {
+//     console.log(array.index);
+//   }
+// }
+// let soma = 0
+// for (let index = 0; index < array.length; index++) {
+//   soma += array[index]
+  
+// }
+// console.log(soma);
+
+// let contador = 0
+// for (let index = 100; index > 5; index - 5) {
+//   contador ++
+// }
+// // console.log(contador);
+
+// let arrayIdades = [12, 13, 16];
+// let idadeMenor
+
+// for (let index = 0; index < arrayIdades.length; index++) {
+//   if (arrayIdades[index] < idadeMenor) {
+//     idadeMenor = arrayIdades[index];
+//   }
+  
+// }
